d9eddbb2fbc242566f1fab17779a8453f80761e2
narusei <naruse1wtbdev@gmail.com>
2021-01-06 21:14:01 +0900

    Init firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.ANPAN_FIREBASE_API_KEY,
+  authDomain: process.env.ANPAN_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.ANPAN_FIREBASE_DATEBASE_URL,
+  projectId: process.env.ANPAN_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.ANPAN_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.ANPAN_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.ANPAN_FIREBASE_APP_ID,
+  measurementId: process.env.ANPAN_FIREBASE_MEASUREMENT_ID,
+}
+
+firebase.initializeApp(config);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
