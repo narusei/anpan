826e25b04918eceebc38332f6e9b74032288e67d
narusei <naruse1wtbdev@gmail.com>
2021-02-09 00:20:17 +0900

    fix: change env naming that firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.ANPAN_FIREBASE_API_KEY,
-  authDomain: process.env.ANPAN_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.ANPAN_FIREBASE_DATEBASE_URL,
-  projectId: process.env.ANPAN_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.ANPAN_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.ANPAN_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.ANPAN_FIREBASE_APP_ID,
-  measurementId: process.env.ANPAN_FIREBASE_MEASUREMENT_ID,
-}
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMEIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 firebase.initializeApp(config);
 firebase.analytics();
