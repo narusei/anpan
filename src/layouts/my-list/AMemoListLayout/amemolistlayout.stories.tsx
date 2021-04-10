14ebde7845b270a409f378b6d20cdfe23c4a5a95
narusei <naruse1wtbdev@gmail.com>
2021-04-10 20:40:54 +0900

    feat: add memo list page

---
+++ src/layouts/my-list/AMemoListLayout/amemolistlayout.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { AMemoListLayout } from '.';
+
+export default {
+  title: 'layout/MemoList',
+  component: AMemoListLayout,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story = (args) => <AMemoListLayout {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ src/pages/my-list/MemoListPage/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AMemoListLayout } from '../../../layouts/my-list/AMemoListLayout';
+
+export const MemoListPage: React.FC = () => {
+  return <AMemoListLayout></AMemoListLayout>;
+};
