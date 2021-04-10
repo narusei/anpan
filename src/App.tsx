8039d3bbea63ce9c31b364792a822e08c7f31b71
narusei <naruse1wtbdev@gmail.com>
2021-04-10 20:38:54 +0900

    feat: add auth page component

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from './auth/AuthProvider';
+import { PrivateRoute } from './auth/PrivateRoute';
+import { AuthenticatedRoute } from './auth/AuthenticatedRoute';
+import { SignInPage } from './pages/SignInPage';
+import { SignUpPage } from './pages/SignUpPage';
 
 function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <Router>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
+      <AuthProvider>
+        <Router>
+          <AuthenticatedRoute exact path="/"></AuthenticatedRoute>
+          <Route exact path="/signin" component={SignInPage}></Route>
+          <Route exact path="/signup" component={SignUpPage}></Route>
+          <PrivateRoute
+            exact
+            path="/my-list"
+            component={MemoListPage}
+          ></PrivateRoute>
             rel="noopener noreferrer"
           >
             Learn React
           </a>
         </header>
-      </Router>
-    </div>
+        </Router>
+      </AuthProvider>
   );
 }
 
