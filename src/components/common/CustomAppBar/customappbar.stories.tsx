c6cd876c79bc886ab57b9ce15dc892dbb4838656
narusei <naruse1wtbdev@gmail.com>
2021-05-28 07:39:27 +0900

    feat: add custom app bar component

---
+++ src/components/common/CustomAppBar/customappbar.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { CustomAppBar, CustomAppBarProps } from '.';
+import { Button } from '@material-ui/core';
+
+export default {
+  title: 'component/common/CustomAppBar',
+  component: CustomAppBar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<CustomAppBarProps> = (args) => <CustomAppBar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  left: (
+    <img
+      src="/static/anpan-logo.svg"
+      alt="Anpan"
+      title="Anpan アンパン"
+      width="94px"
+      height="100%"
+    ></img>
+  ),
+  right: (
+    <>
+      <Button color="inherit">サインイン</Button>
+      <Button color="inherit">サインアップ</Button>
+    </>
+  ),
+};
---
+++ src/components/common/CustomAppBar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppBar, Grid, Grid as MuiGrid, Toolbar } from '@material-ui/core';
+import styled from 'styled-components/macro';
+
+export type CustomAppBarProps = {
+  left: JSX.Element;
+  right: JSX.Element;
+};
+
+const LeftGrid = styled(MuiGrid)`
+  flex-grow: 1;
+`;
+
+export const CustomAppBar: React.FC<CustomAppBarProps> = ({ left, right }) => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <LeftGrid>{left}</LeftGrid>
+        <Grid container justify="flex-end">
+          {right}
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
