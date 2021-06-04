028e165ed7e5e6b548c3f1b57a9428427a0bc96a
narusei <naruse1wtbdev@gmail.com>
2021-06-04 11:51:41 +0900

    feat: add spacing container component

---
+++ src/components/common/SpacingContainer/index.tsx
@@ -0,0 +1,13 @@
+import { Grid as MuiGrid } from '@material-ui/core';
+import React, { ComponentProps } from 'react';
+import styled from 'styled-components/macro';
+
+const SpacingGrid = styled(MuiGrid)<{ flex?: boolean }>`
+  flex-grow: ${(props) => (props.flex ? 1 : 0)};
+`;
+
+export const SpacingContainer: React.FC<
+  ComponentProps<typeof MuiGrid> & { flex?: boolean }
+> = (props) => {
+  return <SpacingGrid {...props}></SpacingGrid>;
+};
