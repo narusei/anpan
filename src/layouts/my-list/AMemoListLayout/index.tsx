373a76ad79ab0954e32e1aee59c69316a9d419e1
narusei <naruse1wtbdev@gmail.com>
2021-06-09 16:36:20 +0900

    fix: delete my-list component

--- src/layouts/my-list/AMemoListLayout/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const AMemoListLayout: React.FC = () => {
-  return <div></div>;
-};
--- src/pages/my-list/MemoListPage/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AMemoListLayout } from '../../../layouts/my-list/AMemoListLayout';
-
-export const MemoListPage: React.FC = () => {
-  return <AMemoListLayout></AMemoListLayout>;
-};
